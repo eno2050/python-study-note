e1801d9b56d249438d6ebc57167e9e1f55883793
eno2050 <117908549@qq.com>
2017-09-04 08:04:27 +0800

    demo

---
+++ class/person.py
@@ -0,0 +1,8 @@
+# _*_ coding:utf-8 _*_
+class Person(object):
+    '''我其实就是一个注释,话说我可以写多行吗？为什么写一行都过不了我还想多行注释吗让我
+    是的金沙江大厦酒店哈就是很受打击啊好的就撒谎'''
+    def __init__(self,name,age):
+        '''我就是一个初始化函数'''
+        self.name = name
+        self.age = str(age)
