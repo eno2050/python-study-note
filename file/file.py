44d0cbb364d4fdb01c6114d1b662db03d00e0702
eno2050 <117908549@qq.com>
2017-09-05 08:34:34 +0800

    python test

--- file/file.py
+++ file/file.py
@@ -1,47 +1,23 @@
 # _*_ coding:utf-8 _*_
 
-# 文件就是一个对象，linux任何元素都是文件
-# _*_ coding:utf-8 _*_
 
-# 文件就是一个对象，linux任何元素都是文件
+def testFileFunc():
+    '''
+    用来测试文件打开的模式不同带来的结果
+    '''
+    # keywords = raw_input("请输入您要测试的模式:")
+    # if keywords.lower() == 'r':
+    #     print '我是只读文件'
+    # elif keywords.lower() == 'w':
+    #     print '我是可写文件'
+    # else:
+    #     print '您的输入不正常'
+    fp = open('hello.txt','a+')
+    print fp.read()
+    fp.close()
+    fp = open('hello.txt','a+')
+    fp.write('hello world')
+    fp.close()
 
-#sys.stdin 文件的标准输入
-#sys.stdout 文件的标准输出
-#sys.stderr 文件的标准错误
-#talk is cheaper show me the code
-
-#httpbin.org/ip
-import urllib2
-import urllib
-from json import loads
-
-#获取妹子总数的页码
-def getPages():
-    html = urllib2.urlopen('https://mm.taobao.com/tstar/search/tstar_model.do?'+
-    '_input_charset=utf-8').read().decode('gbk').encode('utf-8')
-    pages = loads(html)['data']['totalPage']
-    return int(pages)
-
-#print getPages() 1450
-
-#获取所有妹子主页的url
-def getGilsList():
-    L = []
-    for i in range(1,2):
-        values = {
-            'currentPage':i,
-        }
-        data = urllib.urlencode(values)
-        req = urllib2.Request('https://mm.taobao.com/tstar/search/tstar_model.do?'+
-        '_input_charset=utf-8',data = data)
-        res = urllib2.urlopen(req)
-        datas = res.read().decode('gbk').encode('utf-8')
-        list = loads(datas)['data']['searchDOList']
-        L = L+list
-    return L
-
-#print getGilsList()[0]['city']
-num = getGilsList()
-#获取单个妹子的id
-for item in num:
-    print item['userId']
+if __name__ == '__main__':
+    testFileFunc()
--- file/test.py
+++
@@ -1,6 +0,0 @@
-#-*- coding:utf-8 -*-
-
-str = "中国人"
-str = unicode(str,"utf-8")
-print type(str)
-print str
